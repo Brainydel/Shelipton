3e95ac496220e08891fb0c300ac06a6e7661fcbc
Brainydel <davidbello998@gmail.com>
2020-12-13 23:07:06 +0100

    shelipton

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!");
+});
+
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
